ade408b045401ef94d96f2905f487bc4936c474e
Omar Ayard <omar100395@hotmail.com>
2021-03-21 14:51:12 -0600

    B2 Created Purchase Repository and Domain Objects

---
+++ src/main/java/com/platzi/market/domain/PurchaseItem.java
@@ -0,0 +1,41 @@
+package com.platzi.market.domain;
+
+public class PurchaseItem
+{
+    private int ProductId;
+    private int quantity;
+    private double total;
+    private boolean active;
+
+    public int getProductId() {
+        return ProductId;
+    }
+
+    public void setProductId(int productId) {
+        ProductId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
